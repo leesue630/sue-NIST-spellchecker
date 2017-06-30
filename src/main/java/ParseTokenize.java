566a6d778fd993cba56206d162600a2207018835
Sue Lee <lee.sue630@gmail.com>
2017-06-30 16:19:51 -0400

    Normalize words for camel case and trim /n from terms

--- src/main/java/ParseTokenize.java
+++ src/main/java/ParseTokenize.java
@@ -3,20 +3,21 @@
  */
 
 import gov.nist.surf2017.sue.config.SpellCheckerConfig;
+import gov.nist.surf2017.sue.spellchecker.SpellCheckException;
 import gov.nist.surf2017.sue.spellchecker.SpellChecker;
-import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
+import org.apache.commons.lang.StringUtils;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.*;
 import java.io.IOException;
-import java.lang.reflect.Array;
+
 import java.util.*;
 
 public class ParseTokenize {
@@ -55,12 +56,70 @@ public class ParseTokenize {
             String typeName = nodeList1.item(i).getParentNode().getParentNode().getAttributes().getNamedItem("type").getNodeValue();
             String description = nodeList1.item(i).getFirstChild().getNodeValue();
             dictionary.put(typeName, description);
-
-            System.out.println(typeName + ":");
-            System.out.println(description);
-            System.out.println("--------------------------------------");
         }
         return dictionary;
+    }
+
+    // tokenize descriptions: split by spaces, remove stop words, normalize camel case
+    private HashMap<String, List<String>> tokenize(HashMap<String, String> dictionary) {
+        HashMap<String, List<String>> tokenizedDictionary = new HashMap<>();
+        String[] tokens;
+        for (Map.Entry<String, String> entry : dictionary.entrySet()){
+            tokens = entry.getValue().split(" ");
+            for (int i = 0; i < tokens.length; i++) {
+                tokens[i] = tokens[i].replace(".", "").replace(",", "").replace("\\(", "").replace("\\)", "").replace("\n", "");
+            }
+            tokenizedDictionary.put(entry.getKey(), normalize(removeStopWords(tokens)));
+        }
+        return tokenizedDictionary;
+    }
+
+    // removes stop words and white space
+    private List<String> removeStopWords(String[] tokens){
+        List<String> revisedTokens = new ArrayList<>();
+        for (String term : tokens){
+            if (!isStopWord(term.toLowerCase())){
+                revisedTokens.add(term);
+            }
+        }
+        return revisedTokens;
+    }
+
+    // checks if term is a stop word
+    private boolean isStopWord(String term){
+        if (StandardAnalyzer.ENGLISH_STOP_WORDS_SET.contains(term)){
+            return true;
+        }
+        return false;
+    }
+
+    // normalizes tokens for camel case
+    private List<String> normalize(List<String> tokens){
+        for (int i = 0; i < tokens.size(); i++){
+            String term = tokens.get(i);
+            if (term != term.toLowerCase()){
+                String[] termArray = StringUtils.splitByCharacterTypeCamelCase(term);
+                tokens.remove(i);
+                for (int j = 0; j < termArray.length; j++){
+                    tokens.add(i+j, termArray[j]);
+                }
+                i+=(termArray.length - 1);
+            }
+        }
+        return tokens;
+    }
+
+    // checks the spelling of terms
+    private void spellCheck(String term) throws SpellCheckException{
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                SpellCheckerConfig.class
+        );
+        SpellChecker spellChecker = applicationContext.getBean(SpellChecker.class);
+        try {
+            spellChecker.check(term.toLowerCase());
+        } catch (SpellCheckException e){
+            System.out.println("Incorrect Spelling! " + term);
+        }
     }
 
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
@@ -71,48 +130,9 @@ public class ParseTokenize {
         XPathFactory xPathFactory = XPathFactory.newInstance();
         XPath xPath = xPathFactory.newXPath();
         HashMap<String, String> dictionary = parser.getDescriptions(document, xPath);
-        HashMap<String, String[]> tokenizedDictionary = parser.tokenize(dictionary);
+        HashMap<String, List<String>> tokenizedDictionary = parser.tokenize(dictionary);
 //        for (key : tokenizedDictionary){
 //            HashMap<String, String> spellMistakes = parser.spellCheck(tokenizedDictionary.(key));
 //        }
-    }
-
-    private HashMap<String, String[]> tokenize(HashMap<String, String> dictionary) {
-        HashMap<String, String[]> tokenizedDictionary = new HashMap<>();
-        String[] tokens;
-        for (Map.Entry<String, String> entry : dictionary.entrySet()){
-            tokens = entry.getValue().split(" ");
-            for (int i = 0; i < tokens.length; i++) {
-                tokens[i] = tokens[i].replace(".", "").replace(",", "").replace("\\(", "").replace("\\)", "");
-            }
-            tokenizedDictionary.put(entry.getKey(), removeStopWords(tokens));
-        }
-        return tokenizedDictionary;
-    }
-
-    private String[] removeStopWords(String[] tokens){
-        List<String> revisedTokens = new ArrayList<>();
-        for (String term : tokens){
-            if (!(isStopWord(term.toLowerCase()))){
-                revisedTokens.add(term);
-            }
-        }
-        return revisedTokens.toArray(new String[revisedTokens.size()]);
-    }
-
-    private boolean isStopWord(String term){
-        if (StandardAnalyzer.ENGLISH_STOP_WORDS_SET.contains(term)){
-            return true;
-        }
-        return false;
-    }
-
-    private HashMap<String, String> spellCheck(String term) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-                SpellCheckerConfig.class
-        );
-        SpellChecker spellChecker = applicationContext.getBean(SpellChecker.class);
-        spellChecker.check(term);
-        return null;
     }
 }
