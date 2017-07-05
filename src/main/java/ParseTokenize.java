ba26f32e5843bbf8d81b52eab015992f212dc880
Sue Lee <lee.sue630@gmail.com>
2017-07-05 15:54:10 -0400

    Added more stop words and now ignores a list of BOD acronyms

--- src/main/java/ParseTokenize.java
+++ src/main/java/ParseTokenize.java
@@ -24,21 +24,27 @@ public class ParseTokenize {
 
     private static final String DIRECTORY = "C:\\Users\\lns16\\Documents\\OAGi-Semantic-Refinement-and-Tooling\\data\\OAGIS_10_3_EnterpriseEdition\\OAGi-BPI-Platform\\org_openapplications_oagis\\10_3\\Model\\BODs";
     private static final String PLATFORM_DIRECTORY = "C:\\Users\\lns16\\Documents\\OAGi-Semantic-Refinement-and-Tooling\\data\\OAGIS_10_3_EnterpriseEdition\\OAGi-BPI-Platform\\org_openapplications_oagis\\10_3\\Model\\Platform\\2_3\\BODs";
-    private static final String TEST_DIRECTORY = "C:\\Users\\lns16\\Documents\\BODTest";
-    private static final String LOGFILE = "./logfile.txt";
+    private static final String SAMPLE_DIRECTORY = "C:\\Users\\lns16\\Documents\\BODSample";
     private XPathFactory xPathFactory;
     private XPath xPath;
     private BufferedWriter bw = null;
     private FileWriter fw = null;
+    private int errorFileCount = 0;
     private static final List<String> EXTENDED_STOP_WORDS = Arrays.asList(
-            "where", "from", "could", "which"
+            "among", "could", "during", "etc", "from", "others", "since", "than", "them", "unapproved", "upon", "versa", "we", "what", "when", "where", "which", "without", "would", "you",
+            // these are not in the WordNet database
+            "reenter", "reseller"
     );
+    private static final List<String> BOD_ACRONYMS = Arrays.asList(
+            "BOD", "WIP", "IST", "MRP", "ERP", "BOM", "RFQ", "ERP", "HRMS", "CSM", "PDM", "OAGIS", "BSR", "PDC", "CMMS", "GL", "OA"
+    );
+
     public ParseTokenize() {
         this.xPathFactory = XPathFactory.newInstance();
         this.xPath = xPathFactory.newXPath();
     }
 
-    public Document parse(File file) throws ParserConfigurationException, IOException, SAXException {
+    public Document parseFile(File file) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         documentBuilderFactory.setNamespaceAware(true);
         DocumentBuilder documentBuilder;
@@ -53,18 +59,26 @@ public class ParseTokenize {
         }
     }
 
+    // returns hash map of type names and documentation
     private HashMap<String, String> getDescriptions(Document doc, XPath xPath) {
         List<String> list = new ArrayList<>();
-//        String xPathStatement = "//first_name";
 
         HashMap<String, String> dictionary = null;
         dictionary = new HashMap<>();
         NodeList nodeList1 = doc.getElementsByTagName("xsd:documentation");
 
         for (int i = 0; i < nodeList1.getLength() && nodeList1.getLength() != 0; i++) {
-            String typeName = nodeList1.item(i).getParentNode().getParentNode().getAttributes().getNamedItem("type").getNodeValue();
-            String description = nodeList1.item(i).getFirstChild().getNodeValue();
-            dictionary.put(typeName, description);
+            String typeName = null;
+            try {
+                typeName = nodeList1.item(i).getParentNode().getParentNode().getAttributes().getNamedItem("type").getNodeValue();
+            } catch (NullPointerException e) {
+                System.err.print("ERROR: no type attribute for ");
+                typeName = nodeList1.item(i).getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
+            } finally {
+                String description = nodeList1.item(i).getFirstChild().getNodeValue();
+                dictionary.put(typeName, description);
+            }
+
         }
         return dictionary;
     }
@@ -72,31 +86,32 @@ public class ParseTokenize {
     // tokenize descriptions: split by spaces, remove stop words, normalize camel case
     private HashMap<String, List<String>> tokenize(HashMap<String, String> dictionary) {
         HashMap<String, List<String>> tokenizedDictionary = new HashMap<>();
-        String[] tokens;
+        List<String> tokens;
         for (Map.Entry<String, String> entry : dictionary.entrySet()) {
-            tokens = entry.getValue().split(" |\\n");
-            for (int i = 0; i < tokens.length; i++) {
-                tokens[i] = tokens[i].replace(".", "").replace(",", "").replaceAll("[()]", "").replaceAll("\n", "");
+            tokens = new LinkedList<>(Arrays.asList(entry.getValue().split(" |\\n")));
+            for (int i = 0; i < tokens.size(); i++) {
+                // need to replace ".,()/·:'-" and "'s"
+                tokens.set(i, tokens.get(i).replaceAll("\\B's\\b", "").replaceAll("\\W", ""));
             }
-            tokenizedDictionary.put(entry.getKey(), normalize(removeStopWords(tokens)));
+            tokenizedDictionary.put(entry.getKey(), removeStopWords(normalize(tokens)));
         }
         return tokenizedDictionary;
     }
 
     // removes stop words and white space
-    private List<String> removeStopWords(String[] tokens) {
+    private List<String> removeStopWords(List<String> tokens) {
         List<String> revisedTokens = new ArrayList<>();
         for (String term : tokens) {
-            if (!isStopWord(term) && !term.equals("")) {
+            if (!isSpecialWord(term) && !term.equals("")) {
                 revisedTokens.add(term);
             }
         }
         return revisedTokens;
     }
 
-    // checks if term is a stop word
-    private boolean isStopWord(String term) {
-        if (StandardAnalyzer.ENGLISH_STOP_WORDS_SET.contains(term.toLowerCase()) || EXTENDED_STOP_WORDS.contains(term.toLowerCase())) {
+    // checks if term is a stop word or BOD Acronym
+    private boolean isSpecialWord(String term) {
+        if (StandardAnalyzer.ENGLISH_STOP_WORDS_SET.contains(term.toLowerCase()) || EXTENDED_STOP_WORDS.contains(term.toLowerCase()) || BOD_ACRONYMS.contains(term)) {
             return true;
         }
         return false;
@@ -107,18 +122,13 @@ public class ParseTokenize {
         for (int i = 0; i < tokens.size(); i++) {
             String term = tokens.get(i);
             if (!term.equals(term.toLowerCase())) {
-                List<String> termArray = new LinkedList<>(Arrays.asList(StringUtils.splitByCharacterTypeCamelCase(term)));
-                if (termArray.size() != 1) {
+                String[] termArray = StringUtils.splitByCharacterTypeCamelCase(term);
+                if (termArray.length != 1) {
                     tokens.remove(i);
-
-                    // removes Of because it is a stop word
-                    if (termArray.contains("Of")) {
-                        termArray.remove(termArray.indexOf("Of"));
+                    for (int j = 0; j < termArray.length; j++) {
+                        tokens.add(i + j, termArray[j]);
                     }
-                    for (int j = 0; j < termArray.size(); j++) {
-                        tokens.add(i + j, termArray.get(j));
-                    }
-                    i += (termArray.size() - 1);
+                    i += (termArray.length - 1);
                 }
             }
         }
@@ -126,6 +136,7 @@ public class ParseTokenize {
     }
 
     // creates hashmap of all spelling mistakes with type names
+    // if type has no typos, key is not added to hash map
     private HashMap<String, List<String>> spellCheck(HashMap<String, List<String>> dictionary) {
         HashMap<String, List<String>> mistakeTypes = new HashMap<String, List<String>>();
         List<String> mispelled;
@@ -139,7 +150,7 @@ public class ParseTokenize {
                 }
             }
 
-            if (!(mispelled.size() == 0)) {
+            if (mispelled.size() != 0) {
                 mistakeTypes.put(entry.getKey(), mispelled);
             }
         }
@@ -161,32 +172,38 @@ public class ParseTokenize {
         }
     }
 
-    private void printSpellingMistakes(File file, ParseTokenize parser){
+    // writes spelling mistakes for a single file
+    // writes nothing if file has no typos
+    private void printSpellingMistakes(File file, ParseTokenize parser) {
         try {
-            bw.write(file.getName());
-            bw.newLine();
-
-            Document document = parser.parse(file);
+            Document document = parser.parseFile(file);
             HashMap<String, String> dictionary = parser.getDescriptions(document, parser.xPath);
             HashMap<String, List<String>> tokenizedDictionary = parser.tokenize(dictionary);
             HashMap<String, List<String>> spellingMistakes = parser.spellCheck(tokenizedDictionary);
 
-            // write Type Name and typos onto log file
-            for (Map.Entry<String, List<String>> entry : spellingMistakes.entrySet()) {
-                bw.write("Type Name: " + entry.getKey());
+            if (spellingMistakes.size() != 0) {
+                errorFileCount++;
+                bw.write(errorFileCount + ". " + file.getName());
                 bw.newLine();
-                for (String mispell : entry.getValue()) {
-                    bw.write(mispell);
+                // write Type Name and typos onto log file
+                for (Map.Entry<String, List<String>> entry : spellingMistakes.entrySet()) {
+                    bw.write("Type Name: " + entry.getKey());
+                    bw.newLine();
+                    for (String mispell : entry.getValue()) {
+                        bw.write(mispell);
+                        bw.newLine();
+                    }
                     bw.newLine();
                 }
+
+                // dashes indicate new file
+                bw.write("--------------------------");
                 bw.newLine();
             }
 
-            // dashes indicate new file
-            bw.write("--------------------------");
-            bw.newLine();
-        } catch (IOException | SAXException | ParserConfigurationException e){
-            System.err.println(e);
+            System.out.println("FILE: " + file.getName() + " DONE");
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            System.err.print(e);
         }
     }
 
@@ -202,24 +219,24 @@ public class ParseTokenize {
                 // write file name
                 parser.printSpellingMistakes(file, parser);
             }
+            System.out.println("DIRECTORY: " + directoryName + " DONE");
+            bw.write(errorFileCount + " files with spelling mistakes.");
+            bw.newLine();
         } catch (IOException e) {
             System.err.println(e);
         }
     }
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        ParseTokenize parser = new ParseTokenize();
+    private void writeLogfile(String logFileName, String directoryName, String directory, ParseTokenize parser) throws IOException {
         try {
-            parser.fw = new FileWriter(LOGFILE);
+            parser.fw = new FileWriter(logFileName);
             parser.bw = new BufferedWriter(parser.fw);
-            parser.printSpellingMistakes(new File("C:\\Users\\lns16\\Documents\\BODTest\\AcknowledgeBOM.xsd"), parser);
-//            parser.loadDirectory("Test Directory ", TEST_DIRECTORY, parser);
-//            parser.loadDirectory("Model/BODs", DIRECTORY, parser);
-//            parser.loadDirectory("Model/Platform/2_3/BODs", PLATFORM_DIRECTORY, parser);
+            // Test single file
+//        parser.printSpellingMistakes(new File("C:\\Users\\lns16\\Documents\\BODTest\\AcknowledgeBOM.xsd"), parser);
+            parser.loadDirectory(directoryName, directory, parser);
+
         } finally {
-
             try {
-
                 if (parser.bw != null)
                     parser.bw.close();
 
@@ -229,7 +246,14 @@ public class ParseTokenize {
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-
         }
+    }
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        ParseTokenize parser = new ParseTokenize();
+
+        parser.writeLogfile("./get_logfile.txt", "Get Schemas", SAMPLE_DIRECTORY, parser);
+//        parser.writeLogfile("./ModelBODs_logfile.txt", "Model/BODs", DIRECTORY, parser);
+//        parser.writeLogfile("./ModelPlatform_logfile.txt", "Model/Platform/2_3/BODs", PLATFORM_DIRECTORY, parser);
     }
 }
