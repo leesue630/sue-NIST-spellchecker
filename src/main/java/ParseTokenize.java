e38a3e53f1141ce43b45c1d379fd46c15439aba7
Sue Lee <lee.sue630@gmail.com>
2017-07-11 14:58:44 -0400

    rewrote getDescriptions function to direct to getCodeName, getDocumentation, or getCCTS depending on child nodes

--- src/main/java/ParseTokenize.java
+++ src/main/java/ParseTokenize.java
@@ -19,6 +19,8 @@ import javax.xml.xpath.*;
 import java.io.*;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ParseTokenize {
 
@@ -28,15 +30,17 @@ public class ParseTokenize {
     private int mispelledFileCount = 0;
     private String currentFileName = "";
     private static final List<String> EXTENDED_STOP_WORDS = Arrays.asList(
-            "against", "although", "among", "amongst", "because", "cannot", "could", "during", "etc", "from", "his", "how", "multi", "others", "per", "pre", "should", "since", "than", "them", "those", "toward", "unapproved", "unless", "until", "upon", "versa", "we", "what", "when", "where", "whether", "which", "whom", "whose", "without", "would", "you", "your",
+            "against", "although", "among", "amongst", "because", "cannot", "could", "during", "etc", "from", "his", "how", "multi", "others", "per", "pre", "should", "since", "than", "them", "those", "toward", "unapproved", "unless", "until", "upon", "versa", "we", "what", "when", "where", "whereas", "whether", "which", "whom", "whose", "without", "would", "you", "your",
             // these are not in the WordNet database
-            "customization", "reenter", "reseller", "geocoded", "geocoding", "proactively", "subline", "via", "asynchronously", "inline", "itself", "something", "meta", "designator",
+            "customization", "reenter", "reseller", "geocoded", "geocoding", "proactively", "subline", "via", "asynchronously", "inline", "itself", "something", "meta", "designator", "unreceived", "proactively", "subassemblies", "financials", "configurator", "incremented",
+            "postcondition", "Schematron",
             // unsure about these words, but they appear often
-            "sublot", "nonconformant", "datetime", "timeperiod", "timestamp", "teardown", "predefined", "datetimes"
+            "sublot", "nonconformant", "datetime", "timeperiod", "timestamp", "teardown", "predefined", "datetimes", "userid", "usergroup"
     );
     private static final List<String> BOD_ACRONYMS = Arrays.asList(
-            "BOD", "WIP", "IST", "MRP", "ERP", "BOM", "RFQ", "ERP", "HRMS", "CSM", "PDM", "OAGIS", "BSR", "PDC", "CMMS", "GL", "OA", "UOM", "XML", "CNC",
-            "RFID", "EDI", "WIPSTATUS", "CHK", "AP", "FDIS", "LTL", "LIMS", "ORIGEF", "PN", "xsd",
+            "BOD", "WIP", "IST", "MRP", "ERP", "BOM", "RFQ", "ERP", "HRMS", "CSM", "PDM", "OAGIS", "BSR", "PDC", "CMMS", "GL", "OA", "UOM", "XML", "CNC", "ASN", "CRM",
+            "RFID", "EDI", "WIPSTATUS", "CHK", "AP", "FDIS", "LTL", "LIMS", "ORIGEF", "PN", "xsd", "ORIGREF", "VRML", "RECEIPTITM", "RECEIPTLN", "DISTRIBUTN",
+            "TRACKINGID", "DHL", "Fedex", "UCC", "SCC", "truckload", "SHIPUNITSEQ", "SHIPUNITTOT", "PRODUCTLINE", "SEARCHTERM", "REQLINE", "USERAREA", "URI",
             "BO" // because BODs becomes BO and Ds when normalized for camel case
     );
     ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
@@ -60,56 +64,79 @@ public class ParseTokenize {
         }
     }
 
-    // TODO: fix
-    private HashMap<String, String> getCCTSDescriptions(Document doc){
-        HashMap<String, String> dictionary = new HashMap<>();
-        NodeList nodeList1 = doc.getElementsByTagName("xsd:documentation");
+    // gets CCTS Description/Definition for single documentation node
+    private Map.Entry<String, String> getCCTSDescriptions(Node node) {
 
-        for (int i = 0; i < nodeList1.getLength() && nodeList1.getLength() != 0; i++) {
-            String CCTS_Name;
-            String CCTS_Description;
+        String CCTS_Name = "Unknown";
+        // tag <ccts_Definition> or <ccts_Description>
+        String CCTS_Definition = "Unknown";
+        Node secondChild;
+        Node fourthChild;
 
-            try {
-                CCTS_Name = nodeList1.item(i).getChildNodes().item(1).getFirstChild().getNodeValue();
-                CCTS_Description = nodeList1.item(i).getChildNodes().item(3).getFirstChild().getNodeValue();
-                dictionary.put("Name: _" + CCTS_Name + "_", CCTS_Description);
-            } catch (NullPointerException typeErr) {
-                System.err.println("ERROR: empty documentation tag: #" + i);
-            }
-        }
-        return dictionary;
-    }
-
-    // TODO: fix
-    private HashMap<String, String> getCCTSDataTypeDescriptions(Document doc){
-        HashMap<String, String> dictionary = new HashMap<>();
-        NodeList nodeList1 = doc.getElementsByTagName("ccts_Definition");
-
-        for (int i = 0; i < nodeList1.getLength() && nodeList1.getLength() != 0; i++) {
-            String CCTS_Name = "unknown name";
-            String CCTS_Definition = nodeList1.item(i).getFirstChild().getNodeValue();
-
-            try {
-                CCTS_Name = nodeList1.item(i).getParentNode().getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
-                String nodeType = nodeList1.item(i).getParentNode().getParentNode().getParentNode().getNodeName();
-            } catch (NullPointerException firstErr) {
-                System.err.println("ERROR: broad name is not at third parent " + i);
-                try {
-                    CCTS_Name = nodeList1.item(i).getParentNode().getParentNode().getParentNode().getParentNode().getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
-                } catch (NullPointerException secondErr) {
-                    System.err.println("ERROR: broad name is not at sixth parent " + i);
-                    System.err.println("ERROR: Failed to find name " + i);
+        try {
+            secondChild = node.getChildNodes().item(1);
+            if (secondChild.getNodeName() == "ccts_Name") {
+                CCTS_Name = secondChild.getFirstChild().getNodeValue();
+                fourthChild = node.getChildNodes().item(3);
+                if (fourthChild.getNodeName() == "ccts_Definition" || fourthChild.getNodeName() == "ccts_Description") {
+                    CCTS_Definition = fourthChild.getFirstChild().getNodeValue();
                 }
-            } finally {
-                dictionary.put("Name: _" + CCTS_Name + "_", CCTS_Definition);
+            } else if (secondChild.getNodeName() == "ccts_Definition" || secondChild.getNodeName() == "ccts_Description") {
+                CCTS_Definition = secondChild.getFirstChild().getNodeValue();
             }
+        } catch (NullPointerException typeErr) {
+            System.err.println("ERROR: while finding CCTS Name/Definition/Description");
         }
-        return dictionary;
+        return new AbstractMap.SimpleEntry<String, String>("CCTS Name: _" + CCTS_Name + "_", CCTS_Definition);
     }
+
+    // for CodeList_CurrencyCode_ISO_7_04.xsd
+    private Map.Entry<String, String> getCodeNameDescriptions(Node node) {
+
+        String value = "Unknown";
+        String codeName = "Unknown";
+
+        try {
+            value = node.getParentNode().getParentNode().getParentNode().getAttributes().getNamedItem("value").getNodeValue();
+            codeName = node.getChildNodes().item(1).getFirstChild().getNodeValue();
+        } catch (NullPointerException typeErr) {
+            System.err.println("ERROR: no CodeName: ");
+        }
+
+        return new AbstractMap.SimpleEntry<String, String>("Value: _" + value + "_", codeName);
+    }
+
+//    // TODO: fix
+//    private HashMap<String, String> getCCTSDataTypeDescriptions(Document doc) {
+//        HashMap<String, String> dictionary = new HashMap<>();
+//        NodeList nodeList1 = doc.getElementsByTagName("ccts_Definition");
+//
+//        for (int i = 0; i < nodeList1.getLength() && nodeList1.getLength() != 0; i++) {
+//            String CCTS_Name = "unknown name";
+//            String CCTS_Definition = nodeList1.item(i).getFirstChild().getNodeValue();
+//
+//            try {
+//                CCTS_Name = nodeList1.item(i).getParentNode().getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
+//                String nodeType = nodeList1.item(i).getParentNode().getParentNode().getParentNode().getNodeName();
+//            } catch (NullPointerException firstErr) {
+//                System.err.println("ERROR: broad name is not at third parent " + i);
+//                try {
+//                    CCTS_Name = nodeList1.item(i).getParentNode().getParentNode().getParentNode().getParentNode().getParentNode().getParentNode().getAttributes().getNamedItem("name").getNodeValue();
+//                } catch (NullPointerException secondErr) {
+//                    System.err.println("ERROR: broad name is not at sixth parent " + i);
+//                    System.err.println("ERROR: Failed to find name " + i);
+//                }
+//            } finally {
+//                dictionary.put("Name: _" + CCTS_Name + "_", CCTS_Definition);
+//            }
+//        }
+//        return dictionary;
+//    }
 
     // creates a hash map of key(element types/names/refs) with documentation
-    private HashMap<String, String> getDescriptions(Document doc){
+    private HashMap<String, String> getDescriptions(Document doc) {
         HashMap<String, String> hashMap = new HashMap<>();
+        Map.Entry<String, String> entry = new AbstractMap.SimpleEntry<String, String>("Unknown", "Unknown");
 
         // nodeList: list of all documentation nodes
         NodeList nodeList = doc.getElementsByTagName("xsd:documentation");
@@ -117,46 +144,64 @@ public class ParseTokenize {
         // iterate through all documentation nodes
         // attempt to find correct parent node with appropriate type, name, or ref attribute
         for (int i = 0; i < nodeList.getLength() && nodeList.getLength() != 0; i++) {
-            Node parentNode;
-            String typeNameRef = null;
-
-            try {
-                parentNode = nodeList.item(i).getParentNode().getParentNode();
-                typeNameRef = getTypeNameRef(parentNode);
-                if (typeNameRef == "Unknown"){ // try going to fourth ancestor to find type, name or ref
-                    System.out.println("Parent not at second level");
-                    parentNode = parentNode.getParentNode().getParentNode();
-                    typeNameRef = getTypeNameRef(parentNode);
-                    if (typeNameRef == "Unknown"){
-                        System.out.println("Parent not at fourth level");
-                    }
-                }
-            } catch (NullPointerException typeErr) {
-                System.err.println("Error while finding parent");
-            } finally {
-                // check to prevent errors when dealing with empty documentation tags
-                if (nodeList.item(i).hasChildNodes() != false) {
-                    // find documentation and input entry into hashMap
-                    String description = nodeList.item(i).getFirstChild().getNodeValue();
-                    hashMap.put(typeNameRef, description);
-                } else {
-                    // print if documentation tag is empty
+            Node currentNode = nodeList.item(i);
+            int childCount = currentNode.getChildNodes().getLength();
+            switch (childCount) {
+                case 0:
                     System.err.println("ERROR: empty documentation tag: " + currentFileName);
-                }
+                    break;
+                case 1:
+                    entry = getDocumentation(currentNode);
+                    break;
+                default:
+                    String firstChildTagName = currentNode.getChildNodes().item(1).getNodeName();
+                    if (firstChildTagName == "CodeName") {
+                        entry = getCodeNameDescriptions(currentNode);
+                    } else if (firstChildTagName.contains("ccts")) {
+                        entry = getCCTSDescriptions(currentNode);
+                    }
+                    break;
             }
+            hashMap.put(entry.getKey(), entry.getValue()); // typeNameRef, description
         }
         return hashMap;
     }
 
+    private Map.Entry<String, String> getDocumentation(Node node) {
+        Node parentNode;
+        String typeNameRef = null;
+        String description = "Unknown"; // nodeList.item(i).getFirstChild().getNodeValue();
+        try {
+            parentNode = node.getParentNode().getParentNode();
+            typeNameRef = getTypeNameRef(parentNode);
+            if (typeNameRef == "Unknown") { // try going to fourth ancestor to find type, name or ref
+                // System.out.println("Parent not at second level");
+                parentNode = parentNode.getParentNode().getParentNode();
+                typeNameRef = getTypeNameRef(parentNode);
+                if (typeNameRef == "Unknown") {
+                    System.err.println("Parent not at fourth level");
+                }
+            }
+        } catch (NullPointerException typeErr) {
+            System.err.println("Error while finding parent");
+        }
+        try {
+            description = node.getFirstChild().getNodeValue();
+        } catch (NullPointerException typeErr) {
+            System.err.println("Error while finding documentation text");
+        }
+        return new AbstractMap.SimpleEntry<>(typeNameRef, description);
+    }
+
     // Type, Name, or Ref Value formatted
-    private String getTypeNameRef(Node node){
+    private String getTypeNameRef(Node node) {
         String typeNameRef = "Unknown";
         Node attribute = node.getAttributes().getNamedItem("type");
-        if (attribute != null){
+        if (attribute != null) {
             typeNameRef = "Type: _" + attribute.getNodeValue() + "_";
         } else {
             attribute = node.getAttributes().getNamedItem("name");
-            if (attribute != null){
+            if (attribute != null) {
                 typeNameRef = "Name: _" + attribute.getNodeValue() + "_";
             } else {
                 attribute = node.getAttributes().getNamedItem("ref");
@@ -178,13 +223,13 @@ public class ParseTokenize {
             // tokenize using spaces, whitespace, backslash, dash, and underscore
             // \\b: non-word boundary TODO: acts weird here
             // \\B: word boundary
-            tokens = new LinkedList<>(Arrays.asList(entry.getValue().split(" |\\n|(\\b/\\b)|(\\b-\\b)|(\\B_\\B)")));
+            tokens = new LinkedList<>(Arrays.asList(entry.getValue().split(" |(\\b/\\b)|(\\b-\\b)|(\\B_\\B)")));
 
             // cut off non-word characters at the beginning and end of tokens
             // cut off 's and (s) at the end of words
             for (int i = 0; i < tokens.size(); i++) {
                 // need to replace ".,()/·:'-" and "'s"
-                tokens.set(i, tokens.get(i).replaceAll("^\\W|\\W*$", "").replaceAll("('s$)|(\\(s$)", ""));
+                tokens.set(i, tokens.get(i).replaceAll("^\\W|\\W*$", "").replaceAll("('s$)|(\\(s$)", "").trim());
             }
 
             // normalize tokens for camel case and remove special words
@@ -230,10 +275,10 @@ public class ParseTokenize {
         for (String term : tokens) {
 
             // if term is a number
-            if (!isNumeric(term.replaceAll("\\.", ""))) {
+            if (!isNumeric(term.replaceAll("\\.|,", ""))) {
 
-                // if term is not empty, a tab space, a period, or a colon, add term to revised token list
-                if (!isSpecialWord(term) && !term.equals("") && !term.equals("\t") && !term.equals(".") && !term.equals(":")) {
+                // if term is not empty or non-word characters add term to revised token list
+                if (!isSpecialWord(term) && !term.equals("") && !regexChecker("^[\\W]*$", term)) {
                     revisedTokens.add(term);
                 }
             }
@@ -242,14 +287,20 @@ public class ParseTokenize {
     }
 
     // return true if string is numeric
-    public static boolean isNumeric(String str)
-    {
+    public static boolean isNumeric(String str) {
         try {
             double d = Double.parseDouble(str);
-        } catch(NumberFormatException nfe) {
+        } catch (NumberFormatException nfe) {
             return false;
         }
         return true;
+    }
+
+    // return true if regex pattern is found in string
+    public static Boolean regexChecker(String theRegex, String str2Check) {
+        Pattern checkRegex = Pattern.compile(theRegex);
+        Matcher regexMatcher = checkRegex.matcher(str2Check);
+        return regexMatcher.find();
     }
 
     // checks if term is a stop word or BOD Acronym
@@ -301,7 +352,7 @@ public class ParseTokenize {
             Document document = parser.parseFile(file);
             HashMap<String, String> dictionary = parser.getDescriptions(document);
 
-            if (dictionary.size() != 0){ // check for empty document
+            if (dictionary.size() != 0) { // check for empty document
                 HashMap<String, List<String>> tokenizedDictionary = parser.tokenize(dictionary);
                 HashMap<String, List<String>> spellingMistakes = parser.spellCheck(tokenizedDictionary);
 
@@ -379,7 +430,7 @@ public class ParseTokenize {
         String directoryName = "Model/" + suffix;
 
         // create or write over logfile in SpellingMistakes folder
-        String logfileName = "./SpellingMistakes/" + suffix.toLowerCase() + "_logfile.txt";
+        String logfileName = "./SpellingMistakes/" + suffix.toLowerCase().replaceAll("\\\\", "_") + "_logfile.txt";
         parser.writeLogfile(directory, directoryName, logfileName, parser);
     }
 
@@ -410,7 +461,7 @@ public class ParseTokenize {
     }
 
     // write log file for single file
-    private void writeLogfileForSingleFile(String suffix, ParseTokenize parser){
+    private void writeLogfileForSingleFile(String suffix, ParseTokenize parser) {
         String filePath = DIRECTORY_PREFIX + suffix;
         String directoryName = "Model/" + suffix;
         String logfileName = "./SpellingMistakes/" + suffix.toLowerCase().replace(".xsd", "").replaceAll("\\\\", "_") + "_logfile.txt";
@@ -440,6 +491,13 @@ public class ParseTokenize {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         ParseTokenize parser = new ParseTokenize();
 //        parser.writeLogfile("BODs", parser);
-        parser.writeLogfileForSingleFile("BODs\\AcknowledgeBOM.xsd", parser);
+//        parser.writeLogfile("Nouns", parser);
+//        parser.writeLogfile("Platform\\2_3\\BODs", parser);
+//        parser.writeLogfileForSingleFile("Platform\\2_3\\Common\\CodeLists\\CodeLists_1.xsd", parser);
+        parser.writeLogfileForSingleFile("Platform\\2_3\\Common\\CodeLists\\CodeList_CurrencyCode_ISO_7_04.xsd", parser);
+//        parser.writeLogfile("Platform\\2_3\\Common\\CodeLists", parser);
+//        parser.writeLogfile("Platform\\2_3\\Common\\Components", parser);
+//        parser.writeLogfile("Platform\\2_3\\Common\\DataTypes", parser);
+//        parser.writeLogfile("Platform\\2_3\\Common\\IdentifierScheme", parser);
     }
 }
